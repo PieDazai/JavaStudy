4d1360829e3418659d46a894e958df3f5de3b327
1 <kir.it.prog@gmail.com>
2025-02-05 18:23:21 +0700

    only ***********

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.print("**************************************");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите число: ");
 
-        int num = scanner.nextInt();
-        boolean itog = true;
-
-        if (num <= 1) itog = false;
-        else {
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    itog = false;
-                    break;
-                }
-            }
-        }
-        if (itog) System.out.println(num + " - простое число.");
-        else System.out.println(num + " - не простое число.");
     }
 }
